67cbb902c7ca4afa928316b408c6a8ea651d67a0
Emmanium <pradoe.ep@gmail.com>
2020-04-17 18:23:09 -0600

    all tests passing for ex2 in hashtables

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,29 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+    ht = HashTable(length)
+    route = [None] * (length - 1)
 
     """
-    YOUR CODE HERE
+    :tickets: [list] containing Ticket class objects
+    :length: <int>
     """
 
-    pass
+    # insert tickets as source-destination (key-value) into hashtable using hash_table_insert
+    for x in tickets:
+        hash_table_insert(ht, x.source, x.destination)
+    # build list using retrieve, use "NONE" as the initial value
+    curr_location = hash_table_retrieve(ht, "NONE")
+    # set index to 0 for while loop
+    index = 0
+    # since the route list doesn't include the initial location as part of it's size, make sure to subract 1 from the length total
+    while index < length - 1:
+        # since that index to be the current location
+        route[index] = curr_location
+        # find the next destination
+        next_destination = hash_table_retrieve(ht, curr_location)
+        # set the current location to the next destination
+        curr_location = next_destination
+        # increment the index
+        index += 1
+    return route
